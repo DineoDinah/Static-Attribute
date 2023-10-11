045ad9312d0834a6dbb0c5d42719a7a40f4c8d24
Lenovo-User <Lenovo-User@DESKTOP-RNPFAS5>
2023-10-11 10:11:12 +0200

    Add project files.

---
+++ Static Attribute/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Attribute
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Songs Holiday = new Songs("Holiday", "Green day", 2);
+            Songs Kashmir = new Songs("Kashmir", "Led Zeppelin", 3);
+
+            Console.WriteLine(Songs.songCount);
+            Console.ReadLine();
+        }
+    }
+}
